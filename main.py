eb1e899af2ef5c8aa32df5f280a10a6615d7c45a
Your Name <javierfiestasbotella@gmail.com>
2021-04-19 18:45:49 +0200

    fondo y funcionamiento al 97 git
    cls

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class tres_en_raya():
             if self.jugador1==True:#turno del primer jugador
               if  c.get()==" ":
                 c.set("X")
-                b=Button(self.raiz, textvariable=c,text=c,foreground = "red",bd=3,width=10,height=4,command=lambda:tres.colocar_fichas(x,y,b,c)).grid(padx=10, row=x, column=y)
+                b=Button(self.raiz, textvariable=c,text=c,foreground = "red",bd=3,width=10,height=4,command=lambda:tres.colocar_fichas(x,y,b,c)).grid(padx=3,pady=3, row=x, column=y)
                 self.jugador1=False# termina turno de jugador1
                 self.jugador2=True #dá paso el turno del jugador2
                 self.X=self.X+1
@@ -34,7 +34,7 @@ class tres_en_raya():
             elif self.jugador2==True:
               if c.get()==" ":
                 c.set("O")
-                b=Button(self.raiz, textvariable=c,text=c,foreground = "blue", bd=3,width=10,height=4,command=lambda:tres.colocar_fichas(x,y,b,c)).grid(padx=10, row=x, column=y)
+                b=Button(self.raiz, textvariable=c,text=c,foreground = "blue", bd=3,width=10,height=4,command=lambda:tres.colocar_fichas(x,y,b,c)).grid(padx=3,pady=3, row=x, column=y)
                 self.jugador2=False
                 self.jugador1=True
                 self.O=self.O+1
@@ -57,7 +57,7 @@ class tres_en_raya():
           elif  c.get()==" " and  self.jugador1==True and self.X==2:
             c.set("X")
             self.X=3
-            b=Button(self.raiz, textvariable=c,text=c,foreground = "red",bd=3,width=10,height=4,command=lambda:tres.colocar_fichas(x,y,b,c)).grid(padx=10, row=x, column=y)
+            b=Button(self.raiz, textvariable=c,text=c,foreground = "red",bd=3,width=10,height=4,command=lambda:tres.colocar_fichas(x,y,b,c)).grid(padx=3,pady=3, row=x, column=y)
             self.jugador1=False #termina turno de jugador1
             self.jugador2=True 
             tres.combinaciones_ganadoras("X")
@@ -71,7 +71,7 @@ class tres_en_raya():
           elif c.get()==" " and self.jugador2==True and self.O==2:
             c.set("O")
             self.O=3
-            b=Button(self.raiz, textvariable=c,text=c,foreground = "blue", bd=3,width=10,height=4,command=lambda:tres.colocar_fichas(x,y,b,c)).grid(padx=10, row=x, column=y)
+            b=Button(self.raiz, textvariable=c,text=c,foreground = "blue", bd=3,width=10,height=4,command=lambda:tres.colocar_fichas(x,y,b,c)).grid(padx=3,pady=3, row=x, column=y)
             self.jugador2=False
             self.jugador1=True
             tres.combinaciones_ganadoras("O")
@@ -89,8 +89,11 @@ class tres_en_raya():
           print("algo raro pasa")
       else:
         print("se acabo")
+        Button(self.raiz,text="START",background = "green",foreground = "white",command=tres.start).grid(padx=10,row=5,column=2)
+
     
     def start(self):
+      self.colocacion=True
       self.combinacion=False
       self.jugador1=True
       Button(self.raiz,text="START",background = "gray",foreground = "red",command=tres.start,state=DISABLED).grid(padx=10,row=5,column=2)
@@ -157,7 +160,7 @@ class tres_en_raya():
     def interfaz(self):#pantalla interfaz principal
       self.raiz=Tk()
       self.nombre_usuario="Bienvenido!!!"
-      self.raiz.geometry("320x345+0+0")
+      self.raiz.geometry("345x360+0+0")
       self.turno=StringVar()
       self.texto1=StringVar()
       self.texto2=StringVar()
@@ -169,24 +172,26 @@ class tres_en_raya():
       self.texto8=StringVar()
       self.texto9=StringVar()
 
-
+      self.fondo=PhotoImage(file="pruebas python/tres en raya/v.gif",width=355,height=365)
+      self.lblFondo=Label(self.raiz,image=self.fondo).place(x=0,y=0)
+      
       self.turno.set("presione start")
 
       self.raiz.title(self.nombre_usuario)
-      Label(self.raiz,text='Trés en Raya',bg="black",fg="white",width=30).grid(padx=10, row=0, column=1,columnspan=4)
+      Label(self.raiz,text='3 en Raya',bg="black",fg="white",width=30,font="arcade").grid(padx=10, row=0, column=1,columnspan=4)
       Label(self.raiz,text=" ").grid(padx=10, row=1,column=0)
-      self.b1=Button(self.raiz,textvariable=self.texto1, text=self.texto1,width=10,height=4,command=lambda:tres.colocar_fichas(2,1,self.b1,self.texto1)).grid(padx=10, row=2, column=1)
-      self.b2=Button(self.raiz,textvariable=self.texto2, text=self.texto2,width=10,height=4,command=lambda:tres.colocar_fichas(2,2,self.b2,self.texto2)).grid(padx=10, row=2, column=2)
-      self.b3=Button(self.raiz,textvariable=self.texto3, text=self.texto3,width=10,height=4,command=lambda:tres.colocar_fichas(2,3,self.b3,self.texto3)).grid(padx=10, row=2, column=3)
-      self.b4=Button(self.raiz,textvariable=self.texto4, text=self.texto4,width=10,height=4,command=lambda:tres.colocar_fichas(3,1,self.b4,self.texto4)).grid(padx=10, row=3, column=1)
-      self.b5=Button(self.raiz,textvariable=self.texto5, text=self.texto5,width=10,height=4,command=lambda:tres.colocar_fichas(3,2,self.b5,self.texto5)).grid(padx=10, row=3, column=2)
-      self.b6=Button(self.raiz,textvariable=self.texto6, text=self.texto6,width=10,height=4,command=lambda:tres.colocar_fichas(3,3,self.b6,self.texto6)).grid(padx=10, row=3, column=3)
-      self.b7=Button(self.raiz,textvariable=self.texto7, text=self.texto7,width=10,height=4,command=lambda:tres.colocar_fichas(4,1,self.b7,self.texto7)).grid(padx=10, row=4, column=1)
-      self.b8=Button(self.raiz,textvariable=self.texto8, text=self.texto8,width=10,height=4,command=lambda:tres.colocar_fichas(4,2,self.b8,self.texto8)).grid(padx=10, row=4, column=2)
-      self.b9=Button(self.raiz,textvariable=self.texto9, text=self.texto9,width=10,height=4,command=lambda:tres.colocar_fichas(4,3,self.b9,self.texto9)).grid(padx=10, row=4, column=3)
+      self.b1=Button(self.raiz,textvariable=self.texto1, text=self.texto1,width=10,height=4,command=lambda:tres.colocar_fichas(2,1,self.b1,self.texto1)).grid(padx=3,pady=3, row=2, column=1)
+      self.b2=Button(self.raiz,textvariable=self.texto2, text=self.texto2,width=10,height=4,command=lambda:tres.colocar_fichas(2,2,self.b2,self.texto2)).grid(padx=3,pady=3, row=2, column=2)
+      self.b3=Button(self.raiz,textvariable=self.texto3, text=self.texto3,width=10,height=4,command=lambda:tres.colocar_fichas(2,3,self.b3,self.texto3)).grid(padx=3,pady=3, row=2, column=3)
+      self.b4=Button(self.raiz,textvariable=self.texto4, text=self.texto4,width=10,height=4,command=lambda:tres.colocar_fichas(3,1,self.b4,self.texto4)).grid(padx=3,pady=3, row=3, column=1)
+      self.b5=Button(self.raiz,textvariable=self.texto5, text=self.texto5,width=10,height=4,command=lambda:tres.colocar_fichas(3,2,self.b5,self.texto5)).grid(padx=3,pady=3, row=3, column=2)
+      self.b6=Button(self.raiz,textvariable=self.texto6, text=self.texto6,width=10,height=4,command=lambda:tres.colocar_fichas(3,3,self.b6,self.texto6)).grid(padx=3,pady=3, row=3, column=3)
+      self.b7=Button(self.raiz,textvariable=self.texto7, text=self.texto7,width=10,height=4,command=lambda:tres.colocar_fichas(4,1,self.b7,self.texto7)).grid(padx=3,pady=3, row=4, column=1)
+      self.b8=Button(self.raiz,textvariable=self.texto8, text=self.texto8,width=10,height=4,command=lambda:tres.colocar_fichas(4,2,self.b8,self.texto8)).grid(padx=3,pady=3, row=4, column=2)
+      self.b9=Button(self.raiz,textvariable=self.texto9, text=self.texto9,width=10,height=4,command=lambda:tres.colocar_fichas(4,3,self.b9,self.texto9)).grid(padx=3,pady=3, row=4, column=3)
       Label(self.raiz,text="").grid(padx=10)
       Button(self.raiz,text="START",background = "green",foreground = "white",command=tres.start).grid(padx=10,row=5,column=2)
-      self.t=Label(self.raiz,textvariable=self.turno,text=self.turno).grid(row=6,column=2)
+      self.t=Label(self.raiz,textvariable=self.turno,text=self.turno,font="arcade").grid(row=6,column=2)
          
       self.raiz.mainloop()
 
